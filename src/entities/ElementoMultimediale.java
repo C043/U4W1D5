9e8c08ffdf726f1e173d35b2298ef20ec4b7be29
C043 <mariofragnitoph@gmail.com>
2024-07-26 09:56:29 +0200

    Adds ElementoMultimediale

---
+++ src/entities/ElementoMultimediale.java
@@ -0,0 +1,9 @@
+package entities;
+
+public abstract class ElementoMultimediale {
+    private String title;
+
+    public ElementoMultimediale(String title) {
+        this.title = title;
+    }
+}
